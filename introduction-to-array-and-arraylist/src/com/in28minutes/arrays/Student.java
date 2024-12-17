9d18625af8b6f7f15dbc00cddb81b941e6cfb56f
Data-MaSTeRR <hyunwoo8504@gmail.com>
2024-12-17 23:47:09 +0900

    Refactor: Array -> ArrayList, 제네릭, 컬렉션 ...

--- introduction-to-array-and-arraylist/src/com/in28minutes/arrays/Student.java
+++ introduction-to-array-and-arraylist/src/com/in28minutes/arrays/Student.java
@@ -2,21 +2,26 @@ package com.in28minutes.arrays;
 
 import java.math.BigDecimal;
 import java.math.RoundingMode;
+import java.util.ArrayList;
+import java.util.Collections;
 
 public class Student {
 
 	private String name;
-	private int[] marks;
+	private ArrayList<Integer> marks = new ArrayList<Integer>(); // ArrayList는 간편법 불가! 
 
 	public Student(String name, int... marks) { // 가변 인수 사용 (인수의 마지막에 와야 인식함) 
 		this.name = name;
-		this.marks = marks;
+		
+		for (int mark:marks) {
+			this.marks.add(mark);
+		}
 	}
 
-	public int getNumberOfMarks() {
-		return marks.length;
+	public int getNumberOfMarks() { 
+		return marks.size();
 	}
-
+ 
 	public int getTotalSumOfMarks() {
 		
 		int sum = 0;	
@@ -28,15 +33,7 @@ public class Student {
 	}
 
 	public int getMaximumMark() {
-		
-		int maximumMark = Integer.MIN_VALUE; // 음수에도 강건하게 함수 사용가능 
-		for (int mark:marks) {
-			if (mark >= maximumMark) {
-				maximumMark = mark;
-			}
-		}
-		
-		return maximumMark;
+		return Collections.max(marks);
 	}
 
 	public int getMinimumMark() {
@@ -56,6 +53,20 @@ public class Student {
 		
 		return new BigDecimal(sum).divide(new BigDecimal(total), 3, RoundingMode.UP);
 		//98.666666666666666 같은 경우 반올림 어디서 할지 설정해야 함 
+	}
+	
+	// String 을 객체가 아닌 이름으로 출력시켜주는 함수 
+	public String toString() {
+		return name + marks;
+	} 
+	
+	
+	public void addNewMark(int mark) {
+		marks.add(mark);
+	}
+
+	public void removeMarkAtIndex(int index) {
+		marks.remove(index);
 	}
 	
 }
--- introduction-to-array-and-arraylist/src/com/in28minutes/arrays/StudentRunner.java
+++ introduction-to-array-and-arraylist/src/com/in28minutes/arrays/StudentRunner.java
@@ -5,9 +5,8 @@ import java.math.BigDecimal;
 public class StudentRunner {
 
 	public static void main(String[] args) {
-		int[] marks = {99, 98, 100};
 		
-		Student student = new Student("Shim", marks); 
+		Student student = new Student("Shim", 97, 98, 99, 100, 100); 
 		
 		int number = student.getNumberOfMarks();
 		System.out.println("number of marks: " + number);
@@ -23,6 +22,11 @@ public class StudentRunner {
 		
 		BigDecimal average = student.getAverageMarks();
 		System.out.println("average of marks: " + average);
+		
+		System.out.println(student);
+		
+		student.addNewMark(35);
+		student.removeMarkAtIndex(5);
 	}
 
 }
