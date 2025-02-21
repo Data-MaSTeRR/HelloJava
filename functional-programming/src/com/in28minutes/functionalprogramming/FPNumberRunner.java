044c6153d37f9f28de51c6692104cba538c5dc4a
Data-MaSTeRR <hyunwoo8504@gmail.com>
2025-02-21 12:05:29 +0900

    Feat: stream과 intStream 이용하는 알고리

--- functional-programming/src/com/in28minutes/functionalprogramming/FPNumberRunner.java
+++ functional-programming/src/com/in28minutes/functionalprogramming/FPNumberRunner.java
@@ -1,6 +1,8 @@
 package com.in28minutes.functionalprogramming;
 
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class FPNumberRunner {
 
@@ -8,17 +10,41 @@ public class FPNumberRunner {
 		
 		List<Integer> numbers = List.of(4, 6, 8, 13, 3, 15);
 		
+		/*
 		numbers.stream()
 				.forEach( element -> System.out.println(element));
 		
 		// 0은 초기값 역할, reduce는 스트림 요소들을 누적하여 하나의 결과값으로 만드는 연산을 수
-		int sum = numbers.stream().reduce(
+		int sum = numbers.stream()
+				.filter(number -> number % 2 == 1)
+				.reduce(
 				0, 
 				(number1, number2) -> number1 + number2 
-				);
+				);*/
+		
+		// 함수형 프로그래밍 메서드 사용 
+		int sum = fpSum(numbers);
 		
 		System.out.println(sum);
+		
+		System.out.println(List.of(23, 12, 34, 53).stream().filter(e -> e%2==0).collect(Collectors.toList()));
+		System.out.println(IntStream.range(1, 11).map(e -> e * e).boxed().collect(Collectors.toList()));
 
 	}
+	
+	// 리스트의 총합 
+	// 함수형 프로그래밍 - stream
+	private static int fpSum(List<Integer> numbers) {
+		return numbers.stream().reduce(0, (number1, number2) -> number1 + number2 );
+	}
+	
+	// 일반 절차적 프로그래밍 
+		private static int normalSum(List<Integer> numbers) {
+			int sum = 0;
+			for (int number:numbers) {
+				sum += number;
+			}
+			return sum;
+		}
 
 }
