20ad21995d869c6ac95a4b1defa49c94caa3c45b
Data-MaSTeRR <hyunwoo8504@gmail.com>
2025-02-21 12:30:15 +0900

    Feat: filter의 람다표현식 해부 - Predicate

---
+++ functional-programming/src/com/in28minutes/functionalprogramming/LambdaBehindTheScreensRunner.java
@@ -0,0 +1,30 @@
+package com.in28minutes.functionalprogramming;
+
+import java.util.List;
+import java.util.function.Predicate;
+
+class EvenNumberPredicate implements Predicate<Integer> {
+
+	@Override
+	public boolean test(Integer number) {
+		return number%2 == 0;
+	}
+	
+}
+
+public class LambdaBehindTheScreensRunner {
+
+	public static void main(String[] args) {
+		
+		// 람다 표현식 사용 
+		List.of(23, 43, 34, 45, 36, 48).stream()
+			.filter(n -> n%2==0)
+			.forEach(e -> System.out.println(e));
+		
+		// filter 람다표현식의 내부로직을 알아보자 
+		List.of(23, 43, 34, 45, 36, 48).stream()
+		.filter(new EvenNumberPredicate())
+		.forEach(e -> System.out.println(e));
+	}
+
+}
