77ae019b0c1eddb4a06c3f77174eed7beb0ba3c3
Data-MaSTeRR <hyunwoo8504@gmail.com>
2025-02-21 12:33:48 +0900

    Feat: forEach의 람다표현식 해부 - Consumer

--- functional-programming/src/com/in28minutes/functionalprogramming/LambdaBehindTheScreensRunner.java
+++ functional-programming/src/com/in28minutes/functionalprogramming/LambdaBehindTheScreensRunner.java
@@ -1,6 +1,7 @@
 package com.in28minutes.functionalprogramming;
 
 import java.util.List;
+import java.util.function.Consumer;
 import java.util.function.Predicate;
 
 class EvenNumberPredicate implements Predicate<Integer> {
@@ -10,6 +11,14 @@ class EvenNumberPredicate implements Predicate<Integer> {
 		return number%2 == 0;
 	}
 	
+}
+
+class SystemOutConsumer implements Consumer<Integer> {
+
+	@Override
+	public void accept(Integer number) {
+		System.out.println(number);
+	}
 }
 
 public class LambdaBehindTheScreensRunner {
@@ -25,6 +34,11 @@ public class LambdaBehindTheScreensRunner {
 		List.of(23, 43, 34, 45, 36, 48).stream()
 		.filter(new EvenNumberPredicate())
 		.forEach(e -> System.out.println(e));
+		
+		// forEach 람다표현식의 내부로직을 알아보자 
+		List.of(23, 43, 34, 45, 36, 48).stream()
+			.filter(n -> n%2==0)
+			.forEach(new SystemOutConsumer());
 	}
 
 }
