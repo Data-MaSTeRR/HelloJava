1a8088f6bd2a3514152343bcd1d16027abad2803
Data-MaSTeRR <hyunwoo8504@gmail.com>
2025-02-21 12:41:28 +0900

    Feat: map 람다표현식 해부 - Function

--- functional-programming/src/com/in28minutes/functionalprogramming/LambdaBehindTheScreensRunner.java
+++ functional-programming/src/com/in28minutes/functionalprogramming/LambdaBehindTheScreensRunner.java
@@ -2,8 +2,10 @@ package com.in28minutes.functionalprogramming;
 
 import java.util.List;
 import java.util.function.Consumer;
+import java.util.function.Function;
 import java.util.function.Predicate;
 
+// 입력값을 보고 참인지 거짓인지 판단 
 class EvenNumberPredicate implements Predicate<Integer> {
 
 	@Override
@@ -13,12 +15,23 @@ class EvenNumberPredicate implements Predicate<Integer> {
 	
 }
 
+// 입력값을 주고 그에 대한 소비를 진행 
 class SystemOutConsumer implements Consumer<Integer> {
 
 	@Override
 	public void accept(Integer number) {
 		System.out.println(number);
 	}
+}
+
+// 입력값에 함수 적용해서 출력값 나옴  
+class NumberSquareMapper implements Function<Integer, Integer> {
+
+	@Override
+	public Integer apply(Integer number) {
+		return number * number;
+	}
+	
 }
 
 public class LambdaBehindTheScreensRunner {
@@ -28,6 +41,7 @@ public class LambdaBehindTheScreensRunner {
 		// 람다 표현식 사용 
 		List.of(23, 43, 34, 45, 36, 48).stream()
 			.filter(n -> n%2==0)
+			.map(n -> n * n)
 			.forEach(e -> System.out.println(e));
 		
 		// filter 람다표현식의 내부로직을 알아보자 
@@ -39,6 +53,13 @@ public class LambdaBehindTheScreensRunner {
 		List.of(23, 43, 34, 45, 36, 48).stream()
 			.filter(n -> n%2==0)
 			.forEach(new SystemOutConsumer());
+		
+		// map 람다표현식의 내부로직을 알아보자 
+		List.of(23, 43, 34, 45, 36, 48).stream()
+		.filter(n -> n%2==0)
+		.map(n -> n * n)
+		.forEach(e -> System.out.println(e));
+		
 	}
 
 }
