292aec2b451b48ccd5b96da9d7ca8dc690b65da4
Data-MaSTeRR <hyunwoo8504@gmail.com>
2025-02-23 00:54:54 +0900

    Feat: Thread 클래스 상속과 Runnable 인터페이스 구현을 통해 Task1, 2, 3 병렬처리

--- Multithreading/src/ThreadBasicRunner.java
+++ Multithreading/src/ThreadBasicRunner.java
@@ -1,8 +1,9 @@
-
+// extends Thread
+// implements Runnable
 
 class Task1 extends Thread {
 	public void run() {
-		System.out.print("Task1 Start\n");
+		System.out.print("Task1 Started\n");
 		
 		for (int i=101; i<=200; i++) {
 			System.out.print(i + " ");
@@ -12,22 +13,36 @@ class Task1 extends Thread {
 	}
 }
 
+class Task2 implements Runnable {
+
+	@Override
+	public void run() {
+		System.out.print("Task2 Started\n");
+		
+		for (int i=201; i<=300; i++) {
+			System.out.print(i + " ");
+		}
+		
+		System.out.print("\nTask2 Done\n");
+	}
+	
+}
 public class ThreadBasicRunner {
 
 	public static void main(String[] args) {
+		
 		//Task1
 		Task1 task1 = new Task1();
 		// task1.run(); -> 직렬 처리 
 		task1.start(); // -> 병렬 처리 
 		
 		//Task2
-		for (int i=201; i<=300; i++) {
-			System.out.print(i + " ");
-		}
-		
-		System.out.print("\nTask2 Done\n");
+		Task2 task2 = new Task2();
+		Thread task2Thread = new Thread(task2); // Runnable 클래스의 객체를 처리하려면, Thread 클래스에 감싸야함 
+		task2Thread.start(); // -> 병렬 처리 
 		
 		//Task3
+		System.out.print("Task3 Started\n");
 		for (int i=301; i<=400; i++) {
 			System.out.print(i + " ");
 		}
