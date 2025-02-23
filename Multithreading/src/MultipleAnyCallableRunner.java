4223d85cd8a980517cf8e79fa83b3a662c0463d7
Data-MaSTeRR <hyunwoo8504@gmail.com>
2025-02-23 22:33:05 +0900

    Feat: invokeAny()를 통해 가장 먼저 실행되는 Thread의 결과 보기

---
+++ Multithreading/src/MultipleAnyCallableRunner.java
@@ -0,0 +1,29 @@
+import java.util.List;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService; // 스레드 풀 관리를 위한 ExecutorService 
+import java.util.concurrent.Executors; // ExecutorService를 생성하기 위한 Executors
+import java.util.concurrent.Future; // 비동기 작업의 결과를 나타내는 Future 인터페이스
+
+public class MultipleAnyCallableRunner {
+
+public static void main(String[] args) throws InterruptedException, ExecutionException {
+		
+		ExecutorService executorService = Executors.newFixedThreadPool(1); // 1개의 스레드를 가진 스레드 풀 생성
+		// ExecutorService executorService = Executors.newFixedThreadPool(3); -> 더 속도가 빨라짐 
+		
+		List<CallableTask> tasks = List.of(
+				new CallableTask("in28Minutes"), // "in28Minutes" 작업 생성
+				new CallableTask("Ranga"),       // "Ranga" 작업 생성
+				new CallableTask("Adam")         // "Adam" 작업 생성
+				);
+		
+		String result = executorService.invokeAny(tasks); // 가장 먼저 나오는 결과를 result로 반환 
+		
+		System.out.println(result);
+		
+		executorService.shutdown(); // 스레드 풀 종료
+
+	}
+
+}
+
