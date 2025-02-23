67c521f71e32b9e9b085510a119fdd93ae078cbb
Data-MaSTeRR <hyunwoo8504@gmail.com>
2025-02-23 15:00:00 +0900

    Feat: Executors.newFixedThreadPool()를 통해 한번에 실행가능한 Thread 수 정하기

--- Multithreading/src/ExecutorServiceRunner.java
+++ Multithreading/src/ExecutorServiceRunner.java
@@ -1,22 +1,37 @@
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
+class Task extends Thread {
+	
+	private int number;
+
+	public Task(int number) {
+		this.number = number;
+	}
+	
+	public void run() {
+		System.out.print("Task" + number + " Started\n");
+		
+		for (int i=number*100+1; i<=number*100+100; i++) {
+			System.out.print(i + " ");
+		}
+		
+		System.out.print("\nTask" + number + " Done\n");
+	}
+}
+
 public class ExecutorServiceRunner {
 
 	public static void main(String[] args) {
 		
-		ExecutorService executorService = Executors.newSingleThreadExecutor(); // Thread 한번에 하나씩 실행! 
-		executorService.execute(new Task1());
-		executorService.execute(new Thread(new Task2()));
-		
-		//Task3 - 메인 메서드에 그냥. Thread executorService에서 실행되는 것은 아님! 
-		System.out.print("Task3 Started\n");
-		for (int i=301; i<=400; i++) {
-			System.out.print(i + " ");
-		}
-		
-		System.out.print("\nTask3 Done");
-		System.out.print("\nMain Done");
+		//ExecutorService executorService = Executors.newSingleThreadExecutor(); // Thread 한번에 하나씩 실행! 
+		ExecutorService executorService = Executors.newFixedThreadPool(3); // 한번에 Thread 3개까지 실행가능! FIFO로 
+		executorService.execute(new Task(1));
+		executorService.execute(new Task(2));
+		executorService.execute(new Task(3));
+		executorService.execute(new Task(4));
+		executorService.execute(new Task(5));
+		executorService.execute(new Task(6));
 		
 		executorService.shutdown();
 				
