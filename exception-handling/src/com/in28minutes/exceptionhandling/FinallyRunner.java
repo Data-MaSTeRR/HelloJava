7278e79225920bc7cdf13d134fe18f35a04c6165
Data-MaSTeRR <hyunwoo8504@gmail.com>
2025-02-24 12:39:34 +0900

    Add: scanner null확인 주석 추가

--- exception-handling/src/com/in28minutes/exceptionhandling/FinallyRunner.java
+++ exception-handling/src/com/in28minutes/exceptionhandling/FinallyRunner.java
@@ -18,7 +18,9 @@ public class FinallyRunner {
 			e.printStackTrace();
 		} finally { // 메모리 누수가 일어나지 않게 예외처리가 발생하더라도 scanner를 close! 
 			System.out.println("Before Scanner Close");
+			if (scanner != null) { // null 상태에서 close() 메서드를 호출하면 NullPointerException이 발생하기 때문
 			scanner.close();
+			}
 		}
 		
 		System.out.println("Just before closing out main");
