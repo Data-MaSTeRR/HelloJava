1ca8b374ae9bd3cd46211a1ed23ad8fc00e58a3c
Data-MaSTeRR <hyunwoo8504@gmail.com>
2025-02-24 18:10:43 +0900

    Feat: 사용자 정의 예외처리 던지기

--- exception-handling/src/com/in28minutes/exceptionhandling/ThrowingExceptionRunner.java
+++ exception-handling/src/com/in28minutes/exceptionhandling/ThrowingExceptionRunner.java
@@ -1,5 +1,11 @@
 package com.in28minutes.exceptionhandling;
 
+class CurrenciesDoNotMatchException extends Exception {
+	public CurrenciesDoNotMatchException(String msg) {
+		super(msg);
+	}
+}
+
 class Amount {
 	private String currency;
 	private int amount;
@@ -10,11 +16,14 @@ class Amount {
 		this.amount = amount;
 	}
 	
-	public void add(Amount that) {
+	public void add(Amount that) throws CurrenciesDoNotMatchException {
 		// 통화가 같지 않을 경우, 오류를 출력하는 예외처리 추가  
 		if(!this.currency.equals(that.currency)) {
-			throw new RuntimeException("Currencies Don't Match " +
+			/*throw new RuntimeException("Currencies Don't Match " +
 					this.currency + " & " + that.currency); // RuntimeException은 checked exception에 해당
+					*/
+			throw new CurrenciesDoNotMatchException("Currencies Don't Match " +
+					this.currency + " & " + that.currency);
 		}
 		
 		this.amount = this.amount + that.amount;
@@ -28,7 +37,7 @@ class Amount {
 
 public class ThrowingExceptionRunner {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws CurrenciesDoNotMatchException {
 		
 		Amount amount1 = new Amount("USD", 10);
 		Amount amount2 = new Amount("EUR", 20);
